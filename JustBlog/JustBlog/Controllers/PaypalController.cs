336fe01f340f7a2430efc3619dd4b5d1930d7910
zulldesign <admin@zulldesign.ml>
2014-10-14 08:50:31 +0800

    l

---
+++ JustBlog/JustBlog/Controllers/PaypalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JustBlog.Controllers
+{
+    public class PaypalController : Controller
+    {
+        //
+        // GET: /Paypal/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
